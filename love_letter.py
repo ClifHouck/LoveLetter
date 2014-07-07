45f0782de9c854ce56c565e7e62c5da41db6f338
ClifHouck <me@clifhouck.com>
2014-07-07 11:13:28 -0500

    4th of July weekend work
    Refactored Strategy class a bit to split decision making into the three main choices
    that players face:
        - Which card to discard
        - Which opponent to target when apply the effects of a discarded card
        - Which card to guess when targeting another player with the Guard
    
    While these are the main choices, there's a lot of work to do in order to expose and record, within
    the Strategy object, all the relevant data needed to properly make optimal decisions,
    but that will be forthcoming.
    
    Split up RandomStrategy class into its component decisions.
    
    Created ExamineDiscardedCardsGuess class which makes a better than random guess by examining
    what has been discarded and then making a random guess amongst what remains unseen. Still
    not optimal, but better than just randomly guessing.
    
    Created LowestDiscard class which always chooses to discard the lower value card in a player's
    hand. Won't lose by discarding the Princess at least.
    
    Amended discard_hand() in the Player class to properly lose/fail the player if they discard
    the Princess.
    
    Fixed discard_hand() to draw the burn card if the deck is empty.
    
    Added function to Player to access their discard pile.
    
    Fixed Game.do_turn() and Game.lose() to properly out the current_player if they are knocked out during
    their own turn.
    
    Added available_targets() function to Game to return targets that are not under protection.
    
    Added some logic at the end of the script to play several games with AI players and tally the results.

--- love_letter.py
+++ love_letter.py
@@ -1,38 +1,87 @@
+import collections
+import copy
 import random
 
 random.seed()
 
 class Strategy(object):
-    def __init__(self):
-        self._last_seen_hand = None
+    def __init__(self, target_strategy, guess_strategy, discard_strategy):
+        self._last_seen_hand   = None
+        self._target_strategy  = target_strategy
+        self._guess_strategy   = guess_strategy
+        self._discard_strategy = discard_strategy
+
+    def get_target(self, player, game):
+        return self._target_strategy.target(player, game)
+
+    def get_guess(self, player, game):
+        return self._guess_strategy.guess(player, game)
+
+    def get_discard(self, player, game):
+        return self._discard_strategy.get_discard(player, game)
 
     def look_at(self, target):
         self._last_seen_hand = {
-                    'target' : target.number,
-                    'hand'   : target.hand_value()
+                    'target'    : target.number,
+                    'hand'      : target.hand_value(),
+                    'turns_ago' : 0
                 }
 
-class RandomStrategy(Strategy):
     def play(self, player, game):
-        hand            = player.hand()
-        card_to_play    = random.randrange(0, len(hand), 1) 
-        card            = hand[card_to_play]
-
-        random_target   = random.choice(game.players()) 
-        random_guess    = random.randrange(1, 8, 1)
-
-        # The player discards the card before it takes effect.
+        card = self.get_discard(player, game)
         player._discard(game, card)
+        card.discard(game   = game,
+                     player = player,
+                     target = self.get_target(player, game),
+                     guess  = self.get_guess(player, game))
 
-        game.log("RandomStrategy: Player " + str(player.number()) + " discarded the " + card.name() + ", target: " + str(random_target.number()) + 
-                 ", guess: " + str(random_guess) + ".") 
+class RandomDiscard(object):
+    def get_discard(self, player, game):
+        return random.choice(player.hand())
 
-        # This actually applies the effect of the card.
-        card.discard(game   = game, 
-                     player = player, 
-                     target = random_target,
-                     guess  = random_guess)
+class RandomTarget(object):
+    def target(self, player, game):
+        return random.choice(game.available_targets(player)) 
 
+class RandomGuess(object):
+    def guess(self, player, game):
+        return random.randrange(2, 8, 1)
+
+class RandomStrategy(Strategy):
+    def __init__(self):
+        super(RandomStrategy, self).__init__(RandomTarget(), RandomGuess(), RandomDiscard())
+
+class ExamineDiscardedCardsGuess(object):
+    def guess(self, player, game):
+        all_discarded_cards = []
+        for player in game.players():
+            all_discarded_cards += player.discard_pile()
+        discarded_cards = [card.number() for card in all_discarded_cards]
+        game.log("ExamineDiscardedCardsGuess: Discard: " + str(discarded_cards))
+
+        canon_deck = [card.number() for card in Deck.CANONICAL_DECK]
+        whats_left = collections.Counter(canon_deck) - collections.Counter(discarded_cards)
+        del whats_left[1]
+        game.log("ExamineDiscardedCardsGuess: " + str(whats_left))
+        guess = whats_left.most_common(1)[0][0]
+        game.log("ExamineDiscardedCardsGuess: Guessing " + str(guess))
+        return guess
+
+class LowestDiscard(object):
+    def get_discard(self, player, game):
+        hand = player.hand()
+
+        card = hand[1]
+        if hand[1].number() > hand[0].number():
+            card = hand[0]
+
+        game.log("LowestDiscard: Discarding " + card.name() + "(" + str(card.number()) + "), since it is lowest card in the player's hand.")
+        return card
+
+class LowestDiscardStrategy(Strategy):
+    def __init__(self):
+        super(LowestDiscardStrategy, self).__init__(RandomTarget(), RandomGuess(), LowestDiscard())
+    
 class Player(object):
     def __init__(self, card, number, strategy):
         self._hand = [card]
@@ -43,8 +92,12 @@ class Player(object):
     def play(self, game):
         drawn_card = game.deck().draw()
         self._hand.append(drawn_card)
-        game.log("Player " + str(self._number) + " drew the " + drawn_card.name() + ".")
-        game.log("Player " + str(self._number) + "'s hand: " + str(self._hand))
+        game.log("Player " + str(self._number) + " drew the " + drawn_card.name() + "(" + str(drawn_card.number()) + ").")
+        hand_text = "["
+        for card in self._hand:
+            hand_text += card.name() + "(" + str(card.number()) + ") "
+        hand_text += "]"
+        game.log("Player " + str(self._number) + "'s hand: " + hand_text)
 
         numbers = []
         for card in self._hand:
@@ -55,7 +108,7 @@ class Player(object):
             7 in numbers):
             for card in self._hand:
                 if card.number() == 7:
-                    self._discard(card)
+                    self._discard(game, card)
                     return
 
         self._strat.play(self, game)
@@ -69,9 +122,25 @@ class Player(object):
         game.log("Player " + str(self.number()) + " discarded the " + card.name() + " card.")
 
     def discard_hand(self, game):
-        self._discard(game, self._hand[0])
+        card = self._hand[0]
+        self._discard(game, card)
+
+        # If the discarded card is the princess this player loses.
+        if card.number() == 8:
+            game.lose(self)
+            return
+
         game.log("Player " + str(self.number()) + " drew a card after discarding their hand.")
-        self._hand.append(game.deck().draw())
+        card = None
+        if game.deck().size() == 0:
+            card = game.draw_burn_card()
+            game.log("Player " + str(self.number()) + " drew the burn card because the deck is empty.")
+        else:
+            card = game.deck().draw()
+        self._hand.append(card)
+
+    def discard_pile(self):
+        return self._discard_pile
 
     def trade(self, target_player):
         self._hand, target_player._hand = target_player._hand, self._hand
@@ -90,6 +159,15 @@ class Player(object):
 
 
 class Card(object):
+    PRINCESS_NUM    = 8
+    SENSEI_NUM      = 7
+    MANIPULATOR_NUM = 6
+    HATAMOTO_NUM    = 5
+    SHUGENJA_NUM    = 4
+    DIPLOMAT_NUM    = 3
+    COURTIER_NUM    = 2
+    GUARD_NUM       = 1
+
     def discard(self, game, player, target, guess):
         pass
 
@@ -171,17 +249,30 @@ class Guard(Card):
         game.guess(target, guess)
 
 class Deck:
+    CANONICAL_DECK = [
+        Princess(),
+        Sensei(),
+        Manipulator(),
+        Hatamoto(), Hatamoto(),
+        Shugenja(), Shugenja(),
+        Diplomat(), Diplomat(),
+        Coutier(),  Coutier(),
+        Guard(), Guard(), Guard(), Guard(), Guard()
+    ]
+
+    # CANONICAL_DECK = [
+    #     Card.PRINCESS_NUM,
+    #     Card.SENSEI_NUM,
+    #     Card.MANIPULATOR_NUM,
+    #     Card.HATAMOTO_NUM, Card.HATAMOTO_NUM,
+    #     Card.SHUGENJA_NUM, Card.SHUGENJA_NUM, 
+    #     Card.DIPLOMAT_NUM, Card.DIPLOMAT_NUM, 
+    #     Card.COURTIER_NUM, Card.COURTIER_NUM,
+    #     Card.GUARD_NUM, Card.GUARD_NUM, Card.GUARD_NUM, Card.GUARD_NUM, Card.GUARD_NUM,
+   #  ]
+
     def __init__(self):
-        self._cards = [
-                    Princess(),
-                    Sensei(),
-                    Manipulator(),
-                    Hatamoto(), Hatamoto(),
-                    Shugenja(), Shugenja(),
-                    Diplomat(), Diplomat(),
-                    Coutier(),  Coutier(),
-                    Guard(), Guard(), Guard(), Guard(), Guard()
-                ]
+        self._cards = copy.deepcopy(Deck.CANONICAL_DECK)
 
     def shuffle(self):
         random.shuffle(self._cards)
@@ -195,19 +286,20 @@ class Deck:
         return len(self._cards)
 
 class Game:
-    def __init__(self, num_players = 2):
+    def __init__(self, players_strategies):
         self._deck          = Deck()
         self._deck.shuffle()
 
         self._players       = []
-        for n in range(num_players):
-            self._players.append(Player(self._deck.draw(), n, RandomStrategy()))
+        for n in range(len(players_strategies)):
+            self._players.append(Player(self._deck.draw(), n, players_strategies[n]))
 
         self._burn_card      = self._deck.draw()
 
         self._losers         = []
         self._protected      = []
         self._log            = []
+        self._current_player_is_out = False
 
     # Don't call unless is_game_over confirms the game is over.
     def winner(self):
@@ -249,6 +341,11 @@ class Game:
         self._log.append(text)
         print text
 
+    def log_error(self, text):
+        error_text = "ERROR: " + text
+        self._log.append(error_text)
+        print error_text
+
     def do_turn(self):
         current_player = self._players.pop(0)
         self.log("Player " + str(current_player.number()) + "'s turn.")
@@ -260,22 +357,38 @@ class Game:
 
         # Allow the current player to play.
         current_player.play(self)
-        self._players.append(current_player)
+
+        # If the current player was knocked out, clean that up here.
+        if self._current_player_is_out:
+            self._losers.append(current_player)
+            self._current_player_is_out = False
+        else:
+            self._players.append(current_player)
         self.log("Player " + str(current_player.number()) + "'s turn ended.")
+
 
     def lose(self, loser):
         self.log("Player " + str(loser.number()) + " is out.")
         for player in self._players:
             if player.number() == loser.number():
+                self.log("Found a match for the loser: " + str(player.number()))
                 self._players.remove(player)
                 self._losers.append(player)
                 return
+        self._current_player_is_out = True 
 
     def deck(self):
         return self._deck
 
     def protect(self, player):
         self._protected.append(player.number())
+
+    def available_targets(self, current_player):
+        targets = [current_player]
+        for player in self._players:
+            if player.number() not in self._protected:
+                targets.append(player)
+        return targets 
 
     def guess(self, target, guess):
         for card in target.hand():
@@ -285,21 +398,44 @@ class Game:
     def status(self):
         output = ("=== Game Status ===\n" +
                   "Players: ")
+
         for player in self._players:
             output += " " + str(player.number())
         output += "\n"
+
+        for player in self._players:
+            output += " " + str(player.number()) + ": "
+            for card in player.hand():
+                output += card.name() + "(" + str(card.number()) + ")"
+            output += "\n"
 
         output += ("Deck size: " + str(self._deck.size()) + "\n" +
                    "=== End Status ===\n")
 
         return output
 
+    def draw_burn_card(self):
+        if self._burn_card is None:
+            game.log_error("Tried to draw a burn card, but it had already been drawn!")
+            return None
+
+        card = self._burn_card
+        self._burn_card = None
+        return card
+
 def play_game():
-    game = Game(num_players = 4)
+    game = Game([RandomStrategy(),
+                 LowestDiscardStrategy()])
     while not game.is_game_over():
         print game.status()
         game.do_turn()
+    return game.winner()
 
-    winner = game.winner()
+win_tablulation = [0, 0, 0, 0]
+for n in range(10000):
+    print "===== Game Begin ====="
+    winner = play_game()
+    win_tablulation[winner.number()] += 1
+    print "=====  Game End  ====="
 
-play_game()
+print win_tablulation
